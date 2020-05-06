3b0e6638f0cf3edf96359d85e12c921fe7096084
Yan Oliveira <oliveira.yan02@gmail.com>
2020-05-06 11:25:47 -0300

    refactor: ensure that all fields have been sent

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,10 +1,26 @@
-// import AppError from '../errors/AppError';
+import AppError from '../errors/AppError';
 
 import Transaction from '../models/Transaction';
 
+interface Request {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
+
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
+  private validateRequest(request: Request): boolean {
+    const { title, value, type, category } = request;
+    const types = ['income', 'outcome'];
+    return !!title && !!value && types.includes(type) && !!category;
+  }
+
+  public async execute(request: Request): Promise<void> {
+    const requestFieldsAreValid = this.validateRequest(request);
+    if (!requestFieldsAreValid) {
+      throw new AppError('Invalid entries');
+    }
   }
 }
 
