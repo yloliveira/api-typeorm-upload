03e14581c8c1f731979b11214c956658d417328e
Yan Oliveira <oliveira.yan02@gmail.com>
2020-05-07 19:27:58 -0300

    refactor: ensure delete not found throws

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -9,6 +9,10 @@ interface Request {
 class DeleteTransactionService {
   public async execute({ id }: Request): Promise<void> {
     const transactionRepository = getCustomRepository(TransactionRepository);
+    const transaction = await transactionRepository.findOne({ id });
+    if (!transaction) {
+      throw new AppError('Transaction not found', 404);
+    }
     await transactionRepository.delete({ id });
   }
 }
