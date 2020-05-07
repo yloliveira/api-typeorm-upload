2a5ebe8564b101f748d47ae9fbc2b4a20adee3d2
Yan Oliveira <oliveira.yan02@gmail.com>
2020-05-07 11:05:00 -0300

    refactor: transaction model fields

--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,17 +1,38 @@
-class Transaction {
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import Category from './Category';
+
+@Entity('transactions')
+export default class Transaction {
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
 }
-
-export default Transaction;
